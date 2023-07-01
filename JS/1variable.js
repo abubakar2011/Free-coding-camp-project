3a12034768eb15fef8861d1582fc72b354dff85e
abubakar2011 <siddia042@gmail.com>
2023-07-01 20:33:50 +0530

    javascript

---
+++ JS/1variable.js
@@ -0,0 +1,35 @@
+
+// var can  be redeclared
+var a = 3
+var a= 33
+console.log(a)
+
+// let cannot be re-assingned but its value can be updated
+let b= 33
+ b=44
+
+console.log(b)
+
+
+// const cannot be reassigned nor its value can be changed
+const c=33
+
+console.log(c)
+// practice code
+
+
+
+// declaring const as the values are not changing
+const co = document.getElementById("co") 
+co.style.textAlign="center"
+
+
+
+let num1= 33
+let num2=44
+
+num1=num2
+num2=num1
+
+console.log(num1,num2)
+
---
+++ JS/script.js
@@ -0,0 +1,33 @@
+
+console.warn("Not to copy from hear")
+
+var para = document.getElementById("para")
+
+document.write(5525 / 6626)
+
+console.error("Don'make a same site by copy & past")
+
+// inner html me html tag daal do
+
+var demo = document.getElementById("demo")
+demo.style.textAlign="center"
+demo.style.fontFamily="'Courier New', Courier, monospace"
+demo.style.fontFamily="'Courier New', Courier, monospace"
+
+var paragraph = document.getElementById("paragraph")
+paragraph.style.cursor="pointer"
+paragraph.style.fontFamily="'Courier New', Courier, monospace"
+
+var container = document.getElementById("container")
+container.style.backgroundColor="red"
+container.style.textAlign="center"
+container.style.cursor="pointer"
+container.style.width="200px"
+container.style.height="50px"
+container.style.border="1px solid #000"
+
+
+var para = document.getElementById("co")
+para.style.fontFamily="'Courier New', Courier, monospace"
+
+
