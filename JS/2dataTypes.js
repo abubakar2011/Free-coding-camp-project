cd03e7763b7738c2d1bc7cf7ad493f96246f65eb
abubakar2011 <siddia042@gmail.com>
2023-07-02 10:34:59 +0530

    DataType

---
+++ JS/2dataTypes.js
@@ -0,0 +1,31 @@
+
+// first variables are given memory then code is executed
+// data types primitive 
+// it will be undefined as no vallue is assigned
+var a 
+
+console.log(a)
+
+// null is a empty value assign by the user
+var b= null
+
+
+// boolean data type 
+ var isHuman = true
+ var isAnimal = false
+
+
+//  number data type
+
+var value = "33"
+
+// string data type
+
+var name = "Saksham Jaiswal"
+var stringValue = "22"
+// when these primitive data type are saved in the memory and they are stored by vaue
+
+console.log(typeof(value),typeof(stringValue))
+
+console.log(value-stringValue)
+
