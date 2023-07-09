d4ad2ff7b9700b67b7e1bf1a547762d684eba670
abubakar2011 <siddia042@gmail.com>
2023-07-09 17:04:35 +0530

    new

---
+++ JS/3typeConversion.js
@@ -0,0 +1,63 @@
+// console.wa("not to copy")
+// type conversion
+
+let num1 ="55"
+let num2="44"
+
+
+// console.log(typeof num1)
+
+// console.log(typeof  Number(num1))
+
+// console.log( Number(num1))
+
+// console.log(55+55)
+
+
+// Boolean | String
+
+
+// console.log(Boolean(" "))
+
+
+// type cconversion string
+
+const num3 = 33
+const num4 = 22
+
+
+console.log("aa"+num4)
+
+// + oerator has two meaning in js 
+// first it is concatenation "33"+"22"="3322"
+// second it is addition  maths
+
+// minus meanus only   maths minus
+
+
+
+// type conversion in practical 
+let res;
+
+res = "3"+4
+console.log(res)
+
+res = '9' + true; 
+console.log(res);
+res= '0' + null; 
+res = 4 - undefined
+console.log(res);
+
+console.log(Number(undefined))
+
+res = '4' - '4'; 
+console.log(res);
+res = '4' * 5;
+console.log(res)
+
+res = '5' - true;
+console.log(res);
+
+res = 10 + false;
+res = 4 + null; 
+console.log(res)
---
+++ JS/4stringProperties.js
@@ -0,0 +1,6 @@
+let myString = 'JavaScript!!!';
+
+console.log(myString.charCodeAt(1))
+let firstArray =[1,2,3,4,55,88,78]
+
+console.log(firstArray[99],"")
