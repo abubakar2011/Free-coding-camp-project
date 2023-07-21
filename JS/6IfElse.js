ab539e0614d4da6e61c60abd8601106b4a2d189b
abubakar2011 <siddia042@gmail.com>
2023-07-21 10:46:00 +0530

    new

---
+++ JS/6IfElse.js
@@ -0,0 +1,46 @@
+console.log("this is js")
+
+// the value which is given in bracket will bechecked
+
+if (true) {
+
+    // if true then upper value will be printed
+    console.log("5 is greater than 0");
+}
+
+else {
+    // if false then this value is printed
+    console.log("5 is less than 0");
+}
+
+
+
+console.log(Boolean(["aa"]))
+const middle = 10
+console.log(middle < 5)
+
+const str="aaa"
+
+console.log(str.length)
+
+if (str.length > 10) {
+    console.log("string length is less than 10")
+}
+
+else {
+    console.log("string length is greater than 10")
+}
+
+
+
+const arr=[1,2,3,4,5]
+
+if (arr.indexOf(5) < 10) {
+    console.log("arr 5 is less than 10")
+}
+
+else {
+    console.log("arr 5 is grater than 10")
+}
+
+console.log(arr.indexOf(5))
